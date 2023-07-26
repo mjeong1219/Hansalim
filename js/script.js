331a525ece929865ea70dfd909167ad37ecb4182
jeong1219 <dst04021@gamil.com>
2023-07-26 18:10:00 +0900

    신상품 작성중.

--- js/script.js
+++ js/script.js
@@ -13,7 +13,55 @@ window.addEventListener("scroll", function () {
     }
 });
 
+// 콤마 기능
+function priceToString(price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// data.json을 로딩. 연결시켜준다.
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(event){
+    const req = event.target;
+    if(req.readyState === XMLHttpRequest.DONE){
+        const str = req.response;
+        // 글자로 온 데이터를 객체로 변환
+        // 글자가 json 규칙대로 만들어진 문자열.
+        // 그러므로 json 글자를 객체로 변환해서 활용한다.
+        let obj = JSON.parse(str);
+
+        VISUAL_ARR = obj.visual;
+        TODAY_GOOD = obj.todaygood;
+        SALE_GOOD = obj.salegood;
+        NEW_GOOD = obj.newgood;
+        
+        showVisual(); // 비주얼을 화면에 배치한다.
+        showTodayGood(); // 오늘의 상품을 화면에 배치
+        showSaleGood(); // 할인 상품을 화면에 배치
+        showNewGood(); // 신상품을 화면에 배치
+    }
+}
+// 자료를 호출한다.
+xhttp.open("GET", "data.json")
+// 웹브라우저 기능 실행 할 수 있도록 요청.
+xhttp.send();
+// 비주얼 슬라이드
+let VISUAL_ARR;
+let visualTag = document.getElementById("data-visual"); // 슬라이드 바로 위 요소
+// 오늘 상품
+let TODAY_GOOD;
+let todayTag = document.getElementById("data-today");
+let todayTag2 = document.getElementById("data-today2");
+// 할인 상품
+let SALE_GOOD;
+let saleTag = document.getElementById("data-sale");
+// 신상품
+let NEW_GOOD;
+let newTag = document.getElementById("data-new");
+let newListTag = document.getElementById("data-new-list");
+
 // ==============================================================================
+
+//// 헤더 ////
 
 // 펼침 목록들 보기 기능
 // 더보기 목록기능
@@ -103,7 +151,8 @@ listToggle(centerBt, centerList);
 // 전체 메뉴 펼침 기능
 const allMenuArea = document.querySelector(".all-menu-area");
 const allMenu = document.querySelector(".all-menu");
-const cateList = document.querySelector(".cate-list");
+const cateList = document.querySelector(".all-menu-cate");
+// ul인 cate-list로 선언하니 스크롤 부분에 커서올리면 메뉴 사라짐.
 const cateListWrap = document.querySelector(".all-menu-cate-wrap");
 
 cateList.addEventListener("mouseleave", function(){
@@ -135,35 +184,224 @@ cateLists.forEach(function(item,index){
 
 // =====================///////////========================================
 
-// data.json을 로딩
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(event){
-    const req = event.target
-    if(req.readyState === XMLHttpRequest.DONE);
-    const str = req.response;
-    // 글자로 온 데이터를 객체로 변환
-    // 글자가 json 규칙대로 만들어진 문자열.
-    // 그러므로 json 글자를 객체로 변환해서 활용한다.
-    let obj = JSON.parse(str)
 
-    VISUAL_ARR = obj.visual;
-    // 비주얼 화면에 배치한다.
-    showVisual();
-}
+//// 비주얼 ////
 
-// 자료를 호출한다.
-console.log("자료를 가져온다. XMLHT.....")
-xhttp.open("GET", "data.json")
-// 웹브라우저 기능 실행 할 수 있도록 요청.
-xhttp.send();
-
-
-let VISUAL_ARR;
-let visualTag = document.getElementById("data-visual"); // 슬라이드 바로 위 요소
 // 비주얼 화면 출력 기능
 function showVisual(){
     let html = "";
     VISUAL_ARR.forEach(function(item){
-        
+        const tag = `
+        <div class="swiper-slide">
+        <div class="visual-slide-page">
+        <a href="${item.link}">
+        <img src="../images/${item.pic}" alt="${item.name}"/>
+        </a>
+        </div>
+        </div>`;
+        // json의 변수를 가져와서 item으로 선언.
+        html += tag; 
+    });
+    visualTag.innerHTML = html;
+
+
+// 비주얼 슬라이드 기능
+const swVisual = new Swiper(".sw-visual", {
+    loop:true, // loop : 무한으로 도는 것.
+    navigation: {
+        prevEl: ".visual-prev",
+        nextEl: ".visual-next"
+    },
+    autoplay: {
+        delay:2500,
+        disableOnInteraction: false, // 상관없이 계속 autoplay.
+    },
+    pagination: { // 하나하나 넘어가는 것.
+        el: ".visual-pg",
+        type: "fraction"
+    }
+});
+
+// 비주얼 슬라이드 멈춤 기능
+const swVisualPlay = document.querySelector(".visual-play");
+swVisualPlay.addEventListener("click", function(){
+    // 현재 active 클래스가 있는지를 확인하고 기능을 설정한다.
+    // 만약 classList에 contains가 active라면
+    if(swVisualPlay.classList.contains("active")){
+        swVisual.autoplay.start(); // 사진이 멈춰야 하므로 swVisual
+        swVisualPlay.classList.remove("active");
+    }else{
+        swVisual.autoplay.stop(); // autoplay가 멈춤
+        swVisualPlay.classList.add("active"); // active(정지) 됨.
+    }
+});
+}
+
+
+////오늘의 상품////
+
+// 오늘의 상품 화면 출력 기능
+function showTodayGood() {
+    let htmlTop = "";
+    let htmlBottom = "";
+    const topArr = TODAY_GOOD.filter(function (item, index) {
+        if (index < 4) {
+            return item;
+        }
+    });
+
+    topArr.forEach(function (item) {
+        let tag = `
+        <div class="good-box">
+            <!-- 제품이미지 -->
+            <a href="${item.link}" class="good-img">
+                <img src="../images/${item.pic}" alt="${item.name}">
+                <span class="good-type">${item.tag}</span>
+            </a>
+            <!-- 제품정보 -->
+            <a href="${item.link}" class="good-info">
+                <em>${item.name}</em>(<em>${item.unit}</em>)
+            </a>
+            <!-- 제품가격 -->
+            <a href="${item.link}" class="good-info-price">
+            ${priceToString(item.price)}<em>원</em>
+            </a>
+            <!-- 장바구니 이미지 -->
+            <button class="good-add-cart"></button>
+        </div>
+        `;
+        htmlTop += tag;
+    });
+
+    // 배열의 일부분 인덱스 4~7까지 배열만들기
+    const botArr = TODAY_GOOD.filter(function(item, index){
+        if(index > 3){
+            return item;
+        }
+    });
+    botArr.forEach(function(item){
+        let tag = `
+        <div class="good-box">
+            <!-- 제품이미지 -->
+            <a href="${item.link}" class="good-img">
+                <img src="../images/${item.pic}" alt="${item.name}">
+                <span class="good-type">${item.tag}</span>
+            </a>
+            <!-- 제품정보 -->
+            <a href="${item.link}" class="good-info">
+                <em>${item.name}</em>(<em>${item.unit}</em>)
+            </a>
+            <!-- 제품가격 -->
+            <a href="${item.link}" class="good-info-price">
+                ${priceToString(item.price)}<em>원</em>
+            </a>
+            <!-- 장바구니 이미지 -->
+            <button class="good-add-cart"></button>
+        </div>
+        `;
+        htmlBottom += tag;
     })
+
+    todayTag.innerHTML = htmlTop;
+    todayTag2.innerHTML = htmlBottom;
+}
+
+
+//// 할인 상품 ////
+
+// 할인 상품 화면 출력 기능
+function showSaleGood(){
+    let html = `
+    <div class="swiper sw-sale">
+    <div class="swiper-wrapper">
+    `;
+    
+    SALE_GOOD.forEach(function(item){
+        // today와 똑같은 형태이므로 그대로 가져옴.
+        let tag = `
+        <div class="swiper-slide">
+            <div class="good-box">
+                <!-- 제품이미지 -->
+                <a href="${item.link}" class="good-img">
+                    <img src="../images/${item.pic}" alt="${item.name}">
+                    <span class="good-type">${item.tag}</span>
+                </a>
+                <!-- 제품정보 -->
+                <a href="${item.link}" class="good-info">
+                    <em>${item.name}</em>(<em>${item.unit}</em>)
+                </a>
+                <!-- 제품가격 -->
+                <a href="${item.link}" class="good-info-price">
+                    ${priceToString(item.price)}<em>원</em>
+                </a>
+                <!-- 장바구니 이미지 -->
+                <button class="good-add-cart"></button>
+            </div>
+        </div>
+        `;
+        html += tag;
+    });
+    html += `
+    </div>
+    </div>
+    `;
+    saleTag.innerHTML = html;
+    const swSale = new Swiper(".sw-sale", {
+        slidesPerView: 3, // 보여지는 슬라이드 개수
+        spaceBetween: 16, // 슬라이드 간의 간격
+        slidesPerGroup: 3, // 넘어가는 슬라이드 개수
+        navigation: {
+            prevEl: ".sale .slide-prev",
+            nextEl: ".sale .slide-next"
+        },
+        pagination: {
+            // 페이지 수 출력됨.
+            el: ".sale .slide-pg",
+            type: "fraction" // type을 하지 않으면 점으로 나옴.
+        }
+    })
+}
+
+
+//// 신상품 ////
+
+// 신상품 화면 출력 기능
+function showNewGood(){
+    // 첫번째 화면 출력
+    let obj = NEW_GOOD[0]; // 0번째 인 것을 불러옴
+    let newGoodFirst = `
+    <a href="${obj.link}" class="new-img">
+        <img src="../images/${obj.pic}" alt="${obj.title}"/>
+    </a>
+    <a href="${obj.link}" class="new-title">
+        ${obj.title}
+    </a>
+    <a href="${obj.link}" class="new-txt">
+        ${obj.txt}
+    </a>
+    `;
+    newTag.innerHTML = newGoodFirst;
+
+    // 나머지 화면 출력 1~4번
+    let html = "";
+    NEW_GOOD.forEach(function(item, index){
+        let tag = "";
+
+        // 0번은 출력했으므로
+        if(index !== 0){
+            let newGoodList = `
+            <a href="${obj.link}" class="new-img">
+                <img src="../images/${obj.pic}" alt="${obj.title}"/>
+            </a>
+            <a href="${obj.link}" class="new-title">
+                ${obj.title}
+            </a>
+            <a href="${obj.link}" class="new-txt">
+                ${obj.txt}
+            </a>
+            `;
+            newTagList.innerHTML = newGoodList;
+        }
+        
+    });
 }
